e6b39b1c642d3bdd22c37c9e3142fd21982d9d8a
ShugThug <149484307+kartikrosn@users.noreply.github.com>
2026-02-27 12:14:12 +0530

    Delete inventory_project directory

--- inventory_project/wsgi.py
+++
@@ -1,8 +0,0 @@
-"""
-WSGI config for inventory_project project.
-"""
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inventory_project.settings')
-application = get_wsgi_application()
